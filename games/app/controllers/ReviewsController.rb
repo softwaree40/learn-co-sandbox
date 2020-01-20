1b463143e57fc8ae77dee44b1df894dfac265c6c
Abiodun   [Wale] Ajagunna <ajagunnaabiodun752@yahoo.com>
2020-01-20 20:25:24 +0000

    i make a great change to my code

--- games/app/controllers/ReviewsController.rb
+++ games/app/controllers/ReviewsController.rb
@@ -11,17 +11,20 @@ class ReviewsController < ApplicationController
      redirect "/games/#{game.id}"
    end
      
-    get "/games/:id/edit" do 
-      game = Game.find(params[:id])
-      @review = Review.find_by(game: game, user: current_user)
+    get "/games/:id/review/edit" do 
+      @review = find_users_review(params[:id])
       erb :"games/edit"
    end
    
-   patch "/games/:id" do
-       game = Game.find(params[:id])
-       review = Review.find_by(game: game, user: current_user)
-       review.update(comment: params[:comment])
-      redirect "/games/#{game.id}"
+   patch "/games/:id/review" do
+      review = find_users_review(params[:id])
+      review.update(comment: params[:comment])
+      redirect "/games/#{review.game.id}"
+   end
+   
+   delete '/games/:id/review/delete' do
+      find_users_review(params[:id]).destroy 
+      redirect "/games/#{params[:id]}"
    end
    
    
@@ -33,6 +36,11 @@ class ReviewsController < ApplicationController
    helpers do 
     def current_user
        User.find_by(id: session[:user_id])
+    end
+    
+    def find_users_review(game_id)
+      game = Game.find(game_id)
+      Review.find_by(game: game, user: current_user)
     end
   end
   
